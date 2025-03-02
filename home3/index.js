1a8c834f9e63857c875e9639aebe5e6b26c6f27d
oleg voronin <oleg83voronin@gmail.com>
2025-03-02 10:00:20 +0300

    add programm

--- home3/index.js
+++ home3/index.js
@@ -1,26 +1,61 @@
+
 const express = require('express');
 
 const app = express();
 
-let count = 0;
-let countAbout= 0;
+const fs = require("fs");
+const path = require("path");
+
+let counters = {
+    main: 0,
+    about: 0
+};
 
 app.get('/', (req, res) => {
-    res.send(`
-    <h1>Главная страница</h1>
-    <a href="/about">Обо мне</a>
-    <p>Колличество запросов: ${++count}</p>
-    `);
+    counters.main ++;
+    fs.writeFile('counters.json', JSON.stringify(counters, null, 2), (err) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log('Счетчик main сохранен');
+        }
+    });
+    res.send(`<h1>Главная страница</h1>
+            <a href="/about">Обо мне</a>
+            <p>Колличество просмотров: ${counters.main}</p>`);
+
 });
 
 app.get('/about', (req, res) => {
-    res.send(`
-    <h1>Обо мне</h1>
-    <a href="/">Главная страница</a>
-    <p>Колличество запросов: ${++countAbout}</p>
-    `);
+    counters.about ++;
+    fs.writeFile('counters.json', JSON.stringify(counters, null, 2), (err) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log('Счетчик about сохранен');
+        }
+    });
+    res.send(`<h1>Обо мне</h1>
+            <a href="/">Главная</a>
+            <p>Колличество просмотров: ${counters.about}</p>`);
+ 
 });
 
-app.listen(3001, () => {
-    console.log('Server is running on port 3001');
+// Загрузка значений счетчиков из файла при старте сервера
+
+
+
+fs.readFile('counters.json', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+    } else {
+        counters = JSON.parse(data);
+        console.log('Загружены значения счетчиков из файла');
+    }
+});
+
+// Запуск сервера
+   
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
 })
