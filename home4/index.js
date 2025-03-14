c9285944c6b111f34f53320f48158e0a4f3db16a
oleg voronin <oleg83voronin@gmail.com>
2025-03-14 10:32:38 +0300

    all

--- home4/index.js
+++ home4/index.js
@@ -25,31 +25,31 @@ app.use(express.json());
 
 app.get('/users', (req, res) => {
 
-    fs.readFile(path.join(__dirname, 'users.json'), 'utf8', (err, body)  => {
+    fs.readFile(path.join(__dirname, 'users.json'), 'utf8', (err, body) => {
         if (err) {
-            console.error(err);
-        } else {
-            
-            users = JSON.parse(body);
-            console.log('Загружены значения счетчиков из файла');
-        }
+            return res.status(500).send({ message: 'Error reading file' });
+         } const users = JSON.parse(body);
+            res.send({users});       
     });
-      
-        
-    res.send({users});
 });
 
 
 app.get('/users/:id', (req, res) => {
-    const userId = +req.params.id;
-    const user = users.find(user => user.id === userId);
-    if (user) {
-  
-        res.send({user});
+
+    fs.readFile(path.join(__dirname, 'users.json'), 'utf8', (err, body) => {
+        if (err) {
+            return res.status(500).send({ message: 'Error reading file' });
+        } const users = JSON.parse(body);
+        const userId = +req.params.id;
+        const user = users.find(user => user.id === userId);
+            if (user) {
         
-    }else {
-        return res.status(404).send({message: 'User not found'});
-    }
+                res.send({user});
+                
+            }else {
+                return res.status(404).send({message: 'User not found'});
+            }
+    });
     
 });
 
@@ -57,28 +57,33 @@ app.post('/users', (req, res) => {
     uniqueId += 1;
 
     const result = userSchema.validate(req.body);
-
-    
     if (result.error) {
         return res.status(400).send({ message: result.error.details[0].message });
     }
 
-            
-    users.push({
-        id: uniqueId,
-        ...req.body
-    });
-    
-    fs.writeFile(path.join(__dirname, "users.json"), JSON.stringify(users, null, 2), (err, body) => {
+    fs.readFile(path.join(__dirname, 'users.json'), 'utf8', (err, body) => {
         if (err) {
             console.error(err);
-        } else {
-            console.log('файл  сохранен');
+            return res.status(500).send({ message: 'Error reading file' });
+        } const users = JSON.parse(body);
+        if (users.some(user => user.id === uniqueId)) {
+            return res.status(409).send({ message: `User with this ID ${uniqueId} already exists` });
+        }else{
+
+            users.push({
+                id: uniqueId,
+                ...req.body
+            });
+            res.send({id: uniqueId});
+            fs.writeFile(path.join(__dirname, 'users.json'), JSON.stringify(users, null, 2), (err) => {
+                if (err) {
+                    console.error(err);
+                    return res.status(500).send({ message: 'Error writing to file' });
+                } 
+                
+            });
         }
     });
-
-    
-    res.send({id: uniqueId});
 });
 
 app.put('/users/:id', (req, res) => {
@@ -87,33 +92,60 @@ app.put('/users/:id', (req, res) => {
     if (result.error) {
         return res.status(400).send({ message: result.error.details[0].message });
     }
-   
-    const userId = +req.params.id;
-    const user = users.find(user => user.id === userId);
-    if (user) {
-        const { firstName, secondName, age, city } = req.body;
-        user.firstName = firstName || user.firstName;
-        user.secondName = secondName || user.secondName;
-        user.age = age || user.age;
-        user.city = city || user.city;
-        res.send({user});
-        
-    }else {
-        return res.status(404).send({message: 'User not found'});
-    }
+
+    fs.readFile(path.join(__dirname,  'users.json'), 'utf8', (err, body) => {
+        if (err) {
+            return res.status(500).send({ message: 'Error reading file' });
+        } const users = JSON.parse(body);
+            const userId = +req.params.id;
+            const user = users.find(user => user.id === userId);
+            if (user) {
+                const { firstName, secondName, age, city } = req.body;
+                user.firstName = firstName || user.firstName;
+                user.secondName = secondName || user.secondName;
+                user.age = age || user.age;
+                user.city = city || user.city;
+                res.send({user});
+                fs.writeFileSync(path.join(__dirname, 'users.json'), JSON.stringify(users, null, 2) ), (err) => {
+                    if (err) {
+                        console.error(err);
+                        return res.status(500).send({ message: 'Error writing to file' });
+                    }
+                }
+                
+            }else {
+                return res.status(404).send({message: 'User not found'});
+            }
+    });
 });
 
 app.delete('/users/:id', (req, res) => {
-    const userId = +req.params.id;
-    const user = users.find(user => user.id === userId);
-    if (user) {
-        const userIndex = users.indexOf(user);
-        users.splice(userIndex, 1);
-        res.send({user});
-        
-    }else {
-        return res.status(404).send({message: 'User not found'});
-    }
+    
+    fs.readFile(path.join(__dirname, 'users.json'), 'utf8', (err, body) => {
+        if (err) {
+            return res.status(500).send({ message: 'Error reading file' });
+        } const users = JSON.parse(body);
+
+        const userId = +req.params.id;
+        const user = users.find(user => user.id === userId);
+
+            if (user) {
+                const userIndex = users.indexOf(user);
+                users.splice(userIndex, 1);
+                res.send({user});
+                
+                fs.writeFileSync(path.join(__dirname, 'users.json'), JSON.stringify(users, null, 2) ), (err) => {
+                    if (err) {
+                        console.error(err);
+                        return res.status(500).send({ message: 'Error writing to file' });
+                    }
+                    
+                };
+            }else {
+                return res.status(404).send({message: 'User not found'});
+            }
+          
+    });
 });
 
 
